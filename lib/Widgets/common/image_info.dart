c32d50c013749393e013857146923dd6fe0b09c5
viyiviyi <viyiviyi@outlook.com>
2024-03-12 19:07:30 +0800

    增加查看图片信息时自动处理base64格式的json内容的功能

--- lib/Widgets/common/image_info.dart
+++ lib/Widgets/common/image_info.dart
@@ -1,9 +1,9 @@
-import 'dart:async';
+import 'dart:convert';
 
-import 'package:encrypt_gallery/core/encrypt_image.datr.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 import 'package:image/image.dart' as img;
 
 class EnImageInfo extends StatefulWidget {
@@ -11,40 +11,50 @@ class EnImageInfo extends StatefulWidget {
   final String psw;
   const EnImageInfo({Key? key, required this.imagePath, required this.psw})
       : super(key: key);
-
   @override
   _EnImageInfoState createState() => _EnImageInfoState();
 }
 
 class _EnImageInfoState extends State<EnImageInfo> {
   Map<String, String> info = {};
-  Completer<LoadResult>? _completer;
   TextStyle contentTextStyle = const TextStyle(
       color: Colors.white54, fontSize: 16, fontWeight: FontWeight.normal);
   TextStyle titleTextStyle = const TextStyle(
       color: Colors.white70, fontSize: 18, fontWeight: FontWeight.bold);
+
   img.Image? image;
 
   @override
   void initState() {
     super.initState();
-    _completer =
-        loadImageProvider(LoadArg(path: widget.imagePath, pwd: widget.psw));
-    _completer?.future.then((result) {
-      image = result.image;
-      if (image == null) return;
-      if (image!.textData != null) {
-        image!.textData!.forEach((key, value) {
+    img.decodeImageFile(widget.imagePath).then((result) {
+      if (result == null) return;
+      image = result;
+      if (result.textData != null) {
+        result.textData!.forEach((key, value) {
           if (key == 'EncryptPwdSha') {
-          } else if (key == 'Dencrypt' && value == 'true') {
+          } else if (key == 'Encrypt' && value == 'pixel_shuffle_2') {
             info['是否加密'] = '是';
           } else {
-            info[key] = value;
+            try {
+              if (value.startsWith('ey')) {
+                var val = utf8.decode(base64.decode(value));
+                var jsonData = json.decode(val);
+                (jsonData as Map<String, dynamic>).forEach((key, value) {
+                  info[key] = value.toString();
+                });
+                info[key] = val;
+              } else {
+                info[key] = value;
+              }
+            } catch (e) {
+              info[key] = value;
+            }
           }
         });
       }
-      if (image!.hasExif) {
-        image!.exif.directories.forEach((key, value) {
+      if (result.hasExif) {
+        result.exif.directories.forEach((key, value) {
           if (info.containsKey(key)) {
             info[key] =
                 '${info[key]!}\n\n${value.data.values.map((e) => e.toString()).join('\n')}';
@@ -57,9 +67,6 @@ class _EnImageInfoState extends State<EnImageInfo> {
 
   @override
   void dispose() {
-    if (_completer != null) {
-      loadImageProviderDisable(widget.imagePath, _completer!);
-    }
     super.dispose();
   }
 
@@ -113,9 +120,8 @@ class _EnImageInfoState extends State<EnImageInfo> {
                     style: contentTextStyle,
                     recognizer: TapGestureRecognizer()
                       ..onTap = () {
-                        Clipboard.setData(ClipboardData(text: value));
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('已复制到剪切板')));
+                        Clipboard.setData(ClipboardData(text: value)).then(
+                            (value) => showToast('已复制到剪切板', context: context));
                       },
                   ),
                 )
